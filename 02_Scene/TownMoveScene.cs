e97b7587560cc1587163b1df1742975f9854d053
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-06 21:48:15 +0900

    refactor: 게임매니저 towns 접근  | Town.cs
    접근 후 현재 타운에 따라서
    Town추출

--- 02_Scene/TownMoveScene.cs
+++ 02_Scene/TownMoveScene.cs
@@ -18,7 +18,7 @@ namespace TeamRPG_17._02_Scene
             Console.WriteLine("─────────────────────────");
             for ( int i = 0; i < length; i++)
             {
-                Console.WriteLine($"{i + 1}. 마을 이름");
+                Console.WriteLine($"{i + 1}. {GameManager.Instance.towns[i].name}");
             }    
             Console.WriteLine("─────────────────────────");
             if (!GameManager.Instance.SceneInputCommand(out int intCommand))
@@ -26,8 +26,9 @@ namespace TeamRPG_17._02_Scene
 
             if(intCommand >= 0 && intCommand < length)
             {
-                /*if (townList[GameManager.Instance.currentTown].CanEnterTown() == false)
-                    return;*/
+                // GameManager에 가지고있는 towns배열, 현재타운번호를 대입해서 플레이어가 입장 할 수 있는지 확인한다.
+                if (GameManager.Instance.towns[GameManager.Instance.currentTown].CanEnterTown() == false)
+                    return;
 
                 GameManager.Instance.currentTown = intCommand -1;
                 GameManager.Instance.ChangeScene(SceneName.LobbyScene);
