6ba792c6d0188461ff3f5bea06b697ce6c18b1ed
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-06 20:24:55 +0900

    feat: TownMoveScene.cs
    도시이동할 수 있는 씬
    
    사용자 입력을 받으면
    사용자의 입력의 따라서 마을이동 기능 추가

--- 02_Scene/TownMoveScene.cs
+++ 02_Scene/TownMoveScene.cs
@@ -10,7 +10,24 @@ namespace TeamRPG_17._02_Scene
     {
         public override void Update()
         {
+            int length = Enum.GetValues(typeof(ItemName)).Length;
+
+            Console.Clear();
+            Console.WriteLine("도시 이름들어갈 곳");
+            Console.WriteLine("이동해주실 도시를 선택해주세요\n");
+            Console.WriteLine("─────────────────────────");
+            for ( int i = 0; i < length; i++)
+                Console.WriteLine($"{i + 1}. 마을 이름");
             
+            Console.WriteLine("─────────────────────────");
+            if (!GameManager.Instance.SceneInputCommand(out int intCommand))
+                return;
+
+            if(intCommand >= 0 && intCommand < length)
+            {
+                // 게임매니저 변수에 담기
+                GameManager.Instance.ChangeScene(SceneName.LobbyScene);
+            }
         }
     }
 }
