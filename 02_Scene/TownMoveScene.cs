bf11bc065692ed1b19d5b804c6a83253d2815bb5
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-06 19:58:08 +0900

    feat: Add TownMoveScene.cs
    스크립트 파일 추가

---
+++ 02_Scene/TownMoveScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRPG_17._02_Scene
+{
+    internal class TownMoveScene : Scene
+    {
+        public override void Update()
+        {
+            
+        }
+    }
+}
