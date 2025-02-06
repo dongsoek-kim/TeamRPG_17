47aafeaf97eefd6be42c8473ef9b47fc4d98e8be
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-06 20:33:18 +0900

    refactor: GameManager에 유저 입력 받기
    GameManager.cs에 있는
    int currentTown변수에 intCommand를 담아줬습니다.

--- 02_Scene/TownMoveScene.cs
+++ 02_Scene/TownMoveScene.cs
@@ -17,15 +17,16 @@ namespace TeamRPG_17._02_Scene
             Console.WriteLine("이동해주실 도시를 선택해주세요\n");
             Console.WriteLine("─────────────────────────");
             for ( int i = 0; i < length; i++)
+            {
                 Console.WriteLine($"{i + 1}. 마을 이름");
-            
+            }    
             Console.WriteLine("─────────────────────────");
             if (!GameManager.Instance.SceneInputCommand(out int intCommand))
                 return;
 
             if(intCommand >= 0 && intCommand < length)
             {
-                // 게임매니저 변수에 담기
+                GameManager.Instance.currentTown = intCommand -1;
                 GameManager.Instance.ChangeScene(SceneName.LobbyScene);
             }
         }
