16f7a8803c0790a12d5728258b5dbc3661d3e475
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-07 09:56:03 +0900

    feat:DataManager
    save/Load를 관리하는 매니저 생성

---
+++ 01_Manager/DataManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRPG_17
+{
+    internal class DataManager//Player,Inventory,Quest의 데이터 저장,불러오기
+    {
+    }
+}
