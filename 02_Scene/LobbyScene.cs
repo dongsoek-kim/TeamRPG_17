1ccaf38913bc6ff59435d720e1a8490dab49cd32
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-07 19:09:51 +0900

    Refactor: " 마을 이름 기입 "
    $"스파르타... =>
    
    $"{GameManager.Instance.currentTown.name}...

--- 02_Scene/LobbyScene.cs
+++ 02_Scene/LobbyScene.cs
@@ -12,7 +12,7 @@ namespace TeamRPG_17
         {
             Console.Clear();
 
-            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine($"{GameManager.Instance.currentTown.name} 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
             Console.WriteLine("─────────────────────────");
             Console.WriteLine("1. 상태보기");
