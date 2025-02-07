d0af28ca0e920ccc04d5694b61741a39b443fb6e
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-07 11:52:27 +0900

    Fix: "판매관련 사용자 입력부분 수정"
    사용자입력에서 현재도시 startItemIdx를 더해서 값보내기

--- 02_Scene/ShopScene.cs
+++ 02_Scene/ShopScene.cs
@@ -123,7 +123,7 @@ namespace TeamRPG_17
                     onSell = false;
                     break;
                 default:
-                    shop.SellItem(intCommand);
+                    shop.SellItem(GameManager.Instance.currentTown.startItemIdx + intCommand);
                     break;
             }
         }
