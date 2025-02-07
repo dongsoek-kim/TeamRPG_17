50c86762a9cb0ba54dd5ff12984d992005a15924
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-07 16:31:43 +0900

    Refactor: " 데이터씬 메서드들 수정 "
    DataManager에서
    LoadData
    Save 불러오기

--- 02_Scene/DataScene.cs
+++ 02_Scene/DataScene.cs
@@ -40,13 +40,7 @@ namespace TeamRPG_17
             Console.WriteLine("데이터 관리");
             Console.WriteLine("데이터를 관리해주는 씬입니다.\n");
             Console.WriteLine("─────────────────────────");
-            for (int i = 0; i < 3; i++) // Data[] 배열의 공간의 길이를 입력
-            {
-                string str = "비어있음";
-                if (datas[i].name != "")
-                    str = string.Format("Lv {0:D2}. {1}", datas[i].level, datas[i].name);
-                Console.WriteLine($"[{str}]"); // Data[] 안에 원소를 가지고와서 플레이어의 "Lv 0. playerName" 출력 예정 NULL이면 비어있음
-            }
+            DataList(); // 현재 들어있는 데이터 출력
             Console.WriteLine("─────────────────────────");
             Console.WriteLine("1. 저장하기");
             Console.WriteLine("2. 불러오기");
@@ -79,10 +73,7 @@ namespace TeamRPG_17
             Console.WriteLine("원하시는 공간을 지정해주세요.\n");
             Console.WriteLine("─────────────────────────");
 
-            for (int i = 0; i < 3; i++) // Data[] 배열의 공간의 길이를 입력
-            {
-                Console.WriteLine($"{i+1}. [비어있음]"); // Data[] 안에 원소를 가지고와서 플레이어의 "Lv 0. playerName" 출력 예정 NULL이면 비어있음
-            }
+            DataList(); // 현재 들어있는 데이터 출력
 
             Console.WriteLine("─────────────────────────");
             Console.WriteLine("\n0. 나가기");
@@ -96,7 +87,11 @@ namespace TeamRPG_17
                     break;
 
                 default:
-                    //if(intCommand - 1 < slos.length) 데이터 저장하기
+                    if(intCommand - 1 < datas.Length)
+                    {
+                        if (CheckSlot(intCommand, "현재 슬롯에 데이터가 남아있습니다.\n정말로 저장을 하시겠습니까?") == 1) { }
+                            //DataManager.SaveGameData(GameManager.Instance.player, GameManager.Instance.player.inventory, );
+                    }
                     break;
             }
         }
@@ -108,10 +103,7 @@ namespace TeamRPG_17
             Console.WriteLine("원하시는 공간을 지정해주세요.\n");
             Console.WriteLine("─────────────────────────");
 
-            for (int i = 0; i < 3; i++) // Data[] 배열의 공간의 길이를 입력
-            {
-                Console.WriteLine($"{i + 1}. [비어있음]"); // Data[] 안에 원소를 가지고와서 플레이어의 "Lv 0. playerName" 출력 예정 NULL이면 비어있음
-            }
+            DataList();
 
             Console.WriteLine("─────────────────────────");
             Console.WriteLine("\n0. 나가기");
@@ -125,7 +117,11 @@ namespace TeamRPG_17
                     break;
 
                 default:
-                    //if(intCommand - 1 < slos.length) 데이터 불러오기
+                    if (intCommand - 1 < datas.Length)
+                    {
+                        if (datas[intCommand - 1].name != null)
+                            DataManager.LoadData(intCommand);
+                    }
                     break;
             }
         }
@@ -137,10 +133,7 @@ namespace TeamRPG_17
             Console.WriteLine("삭제하실 공간을 지정해주세요.\n");
             Console.WriteLine("─────────────────────────");
 
-            for (int i = 0; i < 3; i++) // Data[] 배열의 공간의 길이를 입력
-            {
-                Console.WriteLine($"{i + 1}. [비어있음]"); // Data[] 안에 원소를 가지고와서 플레이어의 "Lv 0. playerName" 출력 예정 NULL이면 비어있음
-            }
+            DataList();
 
             Console.WriteLine("─────────────────────────");
             Console.WriteLine("\n0. 나가기");
@@ -154,9 +147,52 @@ namespace TeamRPG_17
                     break;
 
                 default:
-                    //if(intCommand - 1 < slos.length) 데이터 불러오기
+                    if (intCommand - 1 < datas.Length)
+                    {
+                        if (CheckSlot(intCommand, "현재 슬롯에 데이터가 남아있습니다.\n정말로 삭제를 하시겠습니까?") == 1)
+                        {
+                            Player p = datas[intCommand];
+                            p.name = "";
+                            //DataManager.SaveGameData(p, new Inventory(), ); 덮어씌우기
+                        }
+                    }
                     break;
             }
+        }
+
+        private void DataList()
+        {
+            for (int i = 0; i < datas.Length; i++)
+            {
+                string str = "비어있음";
+                if (datas[i].name != null)
+                    str = string.Format("Lv {0:D2}. {1}", datas[i].level, datas[i].name);
+                Console.WriteLine($"[{str}]"); // Data[] 안에 원소를 가지고와서 플레이어의 "Lv 0. playerName" 출력 예정 NULL이면 비어있음
+            }
+        }
+
+        private int CheckSlot(int slotNumber, string checkMessage)
+        {
+            string pName = datas[slotNumber].name;
+            int checkCommand = 1;
+            if(pName != null)
+            {
+                Console.Clear();
+                Console.WriteLine(checkMessage);
+                Console.WriteLine("1. 확인\n");
+                Console.WriteLine("0. 취소");
+
+                while (true)
+                {
+                    if (GameManager.Instance.SceneInputCommand(out checkCommand))
+                    {
+                        if (checkCommand >= 0 || checkCommand <= 1)
+                            break;
+                    }
+
+                }
+            }
+            return checkCommand;
         }
     }
 }
