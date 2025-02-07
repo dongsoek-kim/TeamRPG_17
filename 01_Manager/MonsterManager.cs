d9d19c387c128cea745cfa1e8f76d6cc51a8f485
jsw981117 <jsw981117@naver.com>
2025-02-07 14:45:14 +0900

    add: 던전 정보 출력 기능 추가

--- 01_Manager/MonsterManager.cs
+++ 01_Manager/MonsterManager.cs
@@ -11,9 +11,18 @@ namespace TeamRPG_17
 
         private List<Monster> monsters = new List<Monster> 
         {
-            new Monster("슬라임", 1, 5, 5, 5), // 0
-            new Monster("고블린", 1, 15, 5, 5), // 1
-            new Monster("거대한 오크", 2, 45, 12, 10), //2
+            new Monster("슬라임", 1, 5, 5, 5),
+            new Monster("고블린", 1, 15, 5, 5),
+            new Monster("새끼 늑대", 1, 45, 8, 10),
+
+            new Monster("거대한 쥐", 2, 60, 10, 12),
+            new Monster("살벌한 거미", 2, 85, 12, 14),
+            new Monster("그림자 골렘", 2, 120, 14, 20),
+
+            new Monster("해골 병사", 3, 100, 19, 15),
+            new Monster("파묻힌 유령", 3, 80, 25, 20),
+            new Monster("폐허의 수호자", 3, 200, 10, 30),
+
 
         };
 
@@ -27,7 +36,7 @@ namespace TeamRPG_17
 
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = rand.Next(0, 3); // rand = 몬스터 종류를 정해주는거 // if  arr[0] = 2
+                arr[i] = rand.Next(0, monsters.Count); // rand = 몬스터 종류를 정해주는거 // if  arr[0] = 2
                 list.Add(monsters[arr[i]]);
             }
 
---
+++ 03_etc/Dungeon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRPG_17
+{
+    public class Dungeon
+    {
+        public string DungeonName { get; set; }
+        public int Level { get; private set; }
+        public string DungeonInfo { get; private set; }
+
+        public Dungeon(string dungeonName, int level, string dungeonInfo)
+        {
+            DungeonName = dungeonName;
+            Level = level;
+            DungeonInfo = dungeonInfo;
+        }
+    }
+}
