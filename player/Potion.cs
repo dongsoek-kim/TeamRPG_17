3636236da90fa903781bfbf8091e0702923f4bef
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-07 10:24:47 +0900

    feat:Potion
    Potionclass 생성
    Enum PosionNum 생성
    Potionclass
            public int health = 30;
            public int str = 5;
            public int dex = 5;
            public int inte = 5;
            public int luk = 5;
    초기값 설정
    
    public void UsePotion을통해
    hp회복, 능력치상승
    
    Relate to:#1 Player의 능력치가 private.

---
+++ player/Potion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRPG_17
+{
+    public class Potion
+    {
+        public int health = 30;
+        public int str = 5;
+        public int dex = 5;
+        public int inte = 5;
+        public int luk = 5;
+
+        public void UsePotion(PosionNum posionNum)
+        {
+            switch (posionNum)
+            {
+                case PosionNum.Health:
+                    {
+                        GameManager.Instance.player.hp = +health;
+                        GameManager.Instance.player.hp = (GameManager.Instance.player.hp >= 100) ? GameManager.Instance.player.hp = 100 : GameManager.Instance.player.hp;
+                        break;
+                    }
+                case PosionNum.str:
+                    //GameManager.Instance.player.str = +str;
+                    break;
+                case PosionNum.dex:
+                    //GameManager.Instance.player.dex = +str;
+                    break;
+                case PosionNum.inte:
+                    //GameManager.Instance.player.inte = +str;
+                    break;
+                case PosionNum.luk:
+                    //GameManager.Instance.player.luk = +str;
+                    break;
+                default: break;
+
+            }
+        }
+    }
+
+}
+
+public enum PosionNum
+{
+    Health,
+    str,
+    dex,
+    inte,
+    luk
+}
