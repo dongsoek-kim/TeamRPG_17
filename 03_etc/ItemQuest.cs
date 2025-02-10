66795012d789fab24b11e94f68bc9959cd5c202a
kaffu <kaffu@naver.com>
2025-02-10 20:06:34 +0900

    Feat: 2개이상 아이템퀘스트 추가
    2개이상의 아이템을 요구하는 퀘스트 구현 완료
    하나라도 부족할시 퀘스트완료 불가능

--- 03_etc/ItemQuest.cs
+++ 03_etc/ItemQuest.cs
@@ -9,25 +9,29 @@ namespace TeamRPG_17
     public class ItemQuest : Quest
     {
         // 퀘스트완료에 필요한 아이템
-        public string questItem;
+        public string[] questItem;
 
         public override void QuestProgress()
         {
-            string canloadItem = "아이템이없습니다.";
-            if (GameManager.Instance.player.inventory.haveItem(questItem))
-                canloadItem = "제출가능";
-            Console.WriteLine($"{questItem} ( {canloadItem} )\n");
+            foreach (string item in questItem)
+            {
+                string canloadItem = "아이템이없습니다.";
+                if (GameManager.Instance.player.inventory.haveItem(item))
+                    canloadItem = "제출가능";
+                Console.WriteLine($"{item} ( {canloadItem} )\n");
+            }
         }
 
         public override bool QuestComplete()
         {
-            if (GameManager.Instance.player.inventory.haveItem(questItem))
+            if(QuestCheck())
             {
-                // 아이템 반납 및 퀘스트 완료
+                // 아이템 반납 및 퀘스트완료
                 questComplete = true;
-                GameManager.Instance.player.inventory.DeleteItem(questItem);
+                foreach(string item in questItem)
+                    GameManager.Instance.player.inventory.DeleteItem(item);
 
-                // 보상지급
+                // 보상 지급
                 GameManager.Instance.player.AddExp(exp);
                 GameManager.Instance.player.gold += gold;
                 if (rewardItem != null)
@@ -48,12 +52,17 @@ namespace TeamRPG_17
 
         public override bool QuestCheck()
         {
-            // 아이템 가지고있으면 true
-            // 없으면 false
-            if (GameManager.Instance.player.inventory.haveItem(questItem))
-                return true;
-            else
+            // 퀘스트 아이템 리스트에서 하나라도 부족하면 false
+            // 퀘스트 아이템 전부 가지고있으면 true
+            for(int i = 0; i < questItem.Length; i++)
+            {
+                if (GameManager.Instance.player.inventory.haveItem(questItem[i]))
+                    continue;
+
                 return false;
+            }
+
+            return true;
         }
     }
 }
