fa60a84ed8f18cf6b99ddc90b5a3ee96240d492c
jsw981117 <jsw981117@naver.com>
2025-02-10 19:22:09 +0900

    feat: add Skill.cs / SkillManager.cs

---
+++ 01_Manager/SkillManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRPG_17
+{
+    internal class SkillManager : Singleton<SkillManager>
+    {
+        private List<Skill> skills = new List<Skill>
+        {
+            new Skill("강타", 1, 125, 35, JobType.Warrior),
+            new Skill("회전 베기", 2, 145, 55, JobType.Warrior),
+
+            new Skill("표창던지기", 1, 115, 20, JobType.Rogue),
+            new Skill("베놈 바이트", 2, 135, 45, JobType.Rogue),
+
+            new Skill("매직 미사일", 1, 145, 50, JobType.Wizard),
+            new Skill("파이어 볼", 2, 180, 75, JobType.Wizard),
+        };
+        public List<Skill> GetSkillList(JobType jobType) 
+        {
+            List<Skill> skillList = skills.Where(s => s.JobType == jobType).ToList();
+            return skillList;
+        }
+    }
+}
