57263aee351d7f255ceb5b2b90a1f638563194b8
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-12 10:00:07 +0900

    refactor:ItemManager->DataManager
    아이템매니저에있던 ItemLoad 메서드를
    데이터매니저에게 옮김

--- 01_Manager/DataManager.cs
+++ 01_Manager/DataManager.cs
@@ -114,6 +114,75 @@ namespace TeamRPG_17
                 Console.WriteLine($"Error saving game data: {ex.Message}");
             }
         }
+        public static void LoadItemsData()
+        {
+            string relativePath = @"..\..\..\Json\";
+            string jsonFile = "ItemData.json";  // JSON 파일명
+            string jsonPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath, jsonFile));
+            try
+            {
+                if (File.Exists(jsonPath))
+                {
+                    string json = File.ReadAllText(jsonPath);
+                    // JSON 데이터를 불러와 아이템 생성
+                    var rawItems = JsonConvert.DeserializeObject<dynamic[]>(json);
+                    for (int i = 0; i < rawItems.Length; i++)
+                    {
+                        var rawItem = rawItems[i];
+                        ItemType itemType = (ItemType)Enum.Parse(typeof(ItemType), rawItem.itemType.ToString());
+
+                        // 아이템 타입에 맞게 생성
+                        if (itemType == ItemType.Armor)
+                        {
+                            int defense = int.Parse(rawItem.Defense.ToString());
+                            int str = int.Parse(rawItem.str.ToString());
+                            int dex = int.Parse(rawItem.dex.ToString());
+                            int inte = int.Parse(rawItem.inte.ToString());
+                            int luk = int.Parse(rawItem.luk.ToString());
+                            Grade grade = (Grade)Enum.Parse(typeof(Grade), rawItem.Grade.ToString());  // 대소문자 구분 안함
+                            EquipSlot equipSlot = (EquipSlot)Enum.Parse(typeof(EquipSlot), rawItem.EquipSlot.ToString());
+                            ItemManager.Instance.items[i] = new Armor(
+                                rawItem.itemName.ToString(),
+                                rawItem.itemDescription.ToString(),
+                                defense,    // int로 변환된 값
+                                str,
+                                dex,
+                                inte,
+                                luk,
+                                equipSlot,
+                                grade
+                            );
+                        }
+                        else if (itemType == ItemType.Weapon)
+                        {
+                            int damage = int.Parse(rawItem.damage.ToString());
+                            int str = int.Parse(rawItem.str.ToString());
+                            int dex = int.Parse(rawItem.dex.ToString());
+                            int inte = int.Parse(rawItem.inte.ToString());
+                            int luk = int.Parse(rawItem.luk.ToString());
+                            Grade grade = (Grade)Enum.Parse(typeof(Grade), rawItem.Grade.ToString());  // 대소문자 구분 안함
+                            EquipSlot equipSlot = (EquipSlot)Enum.Parse(typeof(EquipSlot), rawItem.EquipSlot.ToString());
+                            // Weapon 객체 생성
+                            ItemManager.Instance.items[i] = new Weapon(
+                                rawItem.itemName.ToString(),
+                                rawItem.itemDescription.ToString(),
+                                damage,    // int로 변환된 값
+                                str,
+                                dex,
+                                inte,
+                                luk,
+                                equipSlot,
+                                grade
+                            );
+                        }
+                    }
+                }
+                else
+                {
+                }
+            }
+            catch { }
+        }
 
     }
     public class ItemConverter : JsonConverter
--- 01_Manager/ItemManager.cs
+++ 01_Manager/ItemManager.cs
@@ -13,86 +13,18 @@ namespace TeamRPG_17
         public int[] itemPrice { get; private set; }
         public int itemLength { get; private set; }
         public int equipSlot {  get; private set; }
+        
         public ItemManager()
         {
             itemLength = Enum.GetValues(typeof(ItemName)).Length;
             items = new Item[itemLength];
             itemPrice = new int[itemLength];
-
-            LoadItemsData();
-
             // 아이템을 다 불러오고 초기화
+        }
+        public void ItemPrice()
+        {
             for (int i = 0; i < itemPrice.Length; i++)
                 itemPrice[i] = items[i].price;
-        }
-        public void LoadItemsData()
-        {
-            string relativePath = @"..\..\..\Json\";
-            string jsonFile = "ItemData.json";  // JSON 파일명
-            string jsonPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath, jsonFile));
-            try
-            {
-                if (File.Exists(jsonPath))
-                {
-                    string json = File.ReadAllText(jsonPath);
-                    // JSON 데이터를 불러와 아이템 생성
-                    var rawItems = JsonConvert.DeserializeObject<dynamic[]>(json);
-                    for (int i = 0; i < rawItems.Length; i++)
-                    {
-                        var rawItem = rawItems[i];
-                        ItemType itemType = (ItemType)Enum.Parse(typeof(ItemType), rawItem.itemType.ToString());
-
-                        // 아이템 타입에 맞게 생성
-                        if (itemType == ItemType.Armor)
-                        {
-                            int defense = int.Parse(rawItem.Defense.ToString());
-                            int str = int.Parse(rawItem.str.ToString());
-                            int dex = int.Parse(rawItem.dex.ToString());
-                            int inte = int.Parse(rawItem.inte.ToString());
-                            int luk = int.Parse(rawItem.luk.ToString());
-                            Grade grade = (Grade)Enum.Parse(typeof(Grade), rawItem.Grade.ToString());  // 대소문자 구분 안함
-                            EquipSlot equipSlot = (EquipSlot)Enum.Parse(typeof(EquipSlot), rawItem.EquipSlot.ToString());
-                            items[i] = new Armor(
-                                rawItem.itemName.ToString(),
-                                rawItem.itemDescription.ToString(),
-                                defense,    // int로 변환된 값
-                                str,
-                                dex,
-                                inte,
-                                luk,
-                                equipSlot,
-                                grade
-                            );
-                        }
-                        else if (itemType == ItemType.Weapon)
-                        {
-                            int damage = int.Parse(rawItem.damage.ToString());
-                            int str = int.Parse(rawItem.str.ToString());
-                            int dex = int.Parse(rawItem.dex.ToString());
-                            int inte = int.Parse(rawItem.inte.ToString());
-                            int luk = int.Parse(rawItem.luk.ToString());
-                            Grade grade = (Grade)Enum.Parse(typeof(Grade), rawItem.Grade.ToString());  // 대소문자 구분 안함
-                            EquipSlot equipSlot = (EquipSlot)Enum.Parse(typeof(EquipSlot), rawItem.EquipSlot.ToString());
-                            // Weapon 객체 생성
-                            items[i] = new Weapon(
-                                rawItem.itemName.ToString(),
-                                rawItem.itemDescription.ToString(),
-                                damage,    // int로 변환된 값
-                                str,
-                                dex,
-                                inte,
-                                luk,
-                                equipSlot,
-                                grade
-                            );
-                        }
-                    }
-                }
-                else
-                {
-                }
-            }
-            catch { }
         }
     }
 }
